a6c202c678401b36d54af8728e23f308a5ec0488
Samuellekpor <samuellekpor@gmail.com>
2023-06-14 22:58:18 +0000

    Add error message

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const container = document.querySelector(".container");
 const success_page = document.querySelector(".success-page");
 const submit_btn = document.querySelector(".submit-button");
+const emailInput = document.getElementById("email");
+
