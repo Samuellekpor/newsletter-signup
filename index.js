768d4752d6e4b0f6209a1a364edb02255b04970b
Samuellekpor <samuellekpor@gmail.com>
2023-06-17 22:38:36 +0000

    Fix button working errors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
 const success_page = document.querySelector(".success-page");
 const submit_btn = document.querySelector(".submit-button");
+const error_message = document.querySelector(".error-message");
 const emailInput = document.getElementById("email");
+const emailField = document.querySelector(".the-email");
 const enteredEmail = emailInput.innerHTML;
+const form = document.querySelector(".small-form");
 
 function validateEmail(email) {
   //regular expression for email validation
@@ -11,13 +14,16 @@ function validateEmail(email) {
   return emailRegex.test(email);
 }
 
-submit_btn.addEventListener('click', () => {
+submit_btn.addEventListener('click', (e) => {
   const isValidEmail = validateEmail(enteredEmail);
 
   if(isValidEmail) {
     container.style.display = 'none';
     success_page.style.display = 'block';
+    emailField.innerHTML = enteredEmail;
+    console.log(enteredEmail);
   } else {
-
+    emailInput.className = 'red';
+    error_message.style.display = "block";
   }
 })
