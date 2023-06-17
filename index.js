a4552f68584805311c8e08ea5a906b4232d6f7d7
Samuellekpor <samuellekpor@gmail.com>
2023-06-17 23:12:52 +0000

    Fixed button working errors

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const submit_btn = document.querySelector(".submit-button");
 const error_message = document.querySelector(".error-message");
 const emailInput = document.getElementById("email");
 const emailField = document.querySelector(".the-email");
-const enteredEmail = emailInput.innerHTML;
 const form = document.querySelector(".small-form");
 
 function validateEmail(email) {
   //regular expression for email validation
-  const emailRegex =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return emailRegex.test(email);
 }
 
 submit_btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const enteredEmail = emailInput.value;
   const isValidEmail = validateEmail(enteredEmail);
-
   if(isValidEmail) {
+    //hide the container and show the success_page
     container.style.display = 'none';
     success_page.style.display = 'block';
     emailField.innerHTML = enteredEmail;
-    console.log(enteredEmail);
   } else {
     emailInput.className = 'red';
     error_message.style.display = "block";
